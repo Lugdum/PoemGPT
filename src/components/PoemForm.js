dc58d833c37536228f8a157809aed0360e02d4ec
Victor Buthod <victor.buthod@epita.fr>
2023-06-20 14:41:09 +0200

    Index Css & Poem Form + Css

---
+++ src/components/PoemForm.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import '../styles/PoemForm.css';
+
+const PoemForm = ({ onSubmit }) => {
+  const [language, setLanguage] = useState('');
+  const [length, setLength] = useState('');
+  const [rhymes, setRhymes] = useState(false);
+  const [style, setStyle] = useState('');
+  const [theme, setTheme] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ language, length, rhymes, style, theme });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="input-field">
+          Language:
+          <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+            <option value="English">English</option>
+            <option value="French">French</option>
+            <option value="Spanish">Spanish</option>
+            <option value="German">German</option>
+            <option value="Italian">Italian</option>
+          </select>
+        </label>
+        <label className="input-field">
+          Length:
+          <input
+            type="number"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+            required
+          />
+        </label>
+        <label className="checkbox-field">
+          Rhymes:
+          <input
+            type="checkbox"
+            checked={rhymes}
+            onChange={(e) => setRhymes(e.target.checked)}
+          />
+        </label>
+        <label className="input-field">
+          Style:
+          <input
+            type="text"
+            value={style}
+            onChange={(e) => setStyle(e.target.value)}
+            required
+          />
+        </label>
+        <label className="input-field">
+          Theme:
+          <input
+            type="text"
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">
+          Generate poem
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default PoemForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,23 @@ import { generatePoem } from "../services/poemService";
 import "../styles/Index.css";
 
 export default function Index() {
+  const [poem, setPoem] = useState('');
+
+  const handleFormSubmit = async (formData) => {
+    try {
+      const generatedPoem = await generatePoem(formData);
+      setPoem(generatedPoem);
+    } catch (error) {
+      console.error('Failed to generate poem:', error);
+    }
+  };
+
   return (
     <div className="container">
       <div className="poem-form-container">
-        <div className="form-content">components</div>
+        <div className="form-content">
+          <PoemForm onSubmit={handleFormSubmit} />
+        </div>
       </div>
     </div>
   );
