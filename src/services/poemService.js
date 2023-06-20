f50c513b99a78b9be1ab38eb23475c198bd50f61
Victor Buthod <victor.buthod@epita.fr>
2023-06-20 15:01:42 +0200

    Generate Poem

---
+++ src/services/poemService.js
@@ -0,0 +1,36 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const openai = new OpenAIApi(new Configuration({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY}));
+
+async function generatePoem(formData) {
+  const { language, length, rhymes, style, theme } = formData;
+  let len = parseInt(length);
+
+  const prompt = `Create a ${language} ${rhymes ? 'rhyming ' : ''}poem in the style of ${style} about ${theme} with a length of ${length} words`;
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: prompt,
+      },
+      {
+        role: 'user',
+        content: 'Start poem',
+      }
+    ],
+    max_tokens: len,
+  });
+
+  if (response.data.choices && response.data.choices.length > 0) {
+    console.log(response.data.choices[0].message.content);
+    return response.data.choices[0].message.content;
+  }
+
+  throw new Error('Failed to generate poem');
+}
+
+module.exports = {
+  generatePoem,
+};
